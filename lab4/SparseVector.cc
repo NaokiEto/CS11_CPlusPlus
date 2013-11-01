3953593a18ff560df36bedf426cba3e6159f193f
Lakers <neto@caltech.edu>
2013-11-01 18:09:36 -0400

    more changes to lab 4, first few tests now work

--- lab4/SparseVector.cc
+++ lab4/SparseVector.cc
@@ -6,15 +6,10 @@ SparseVector::SparseVector(int s)
 {
     // make sure that the size of the vector is positive
     assert(s > 0);
-    
+
     size = s;
-    firstnon0 = new node(0, 1);
-    node *curr = firstnon0;
-    for (int i = 0; i < s; i++)
-    {
-        curr->next = new node(i, 1);
-        curr = curr->next;
-    }
+    firstnon0 = 0;
+    //checkListOrder();
 }
 
 // Helper function to delete sparse vector
@@ -25,18 +20,22 @@ void SparseVector::clear()
     while (curr != 0) 
     {
         // Get what is next, before deleting curr.
-        node *next = curr->next;
+        node *nxt = curr->next;
+        // debug
+        printf("the index and value are: %d, %d \n", curr->index, curr->value);
         // Delete this node.
         delete curr;
         // Go to next node in list
-        curr = next;
+        curr = nxt;
     }
+    firstnon0 = 0;
+    size = 0;
 }
 
 // Helper function to copy sparse vector
 void SparseVector::copyList(const SparseVector &sv)
 {
-    size = sv.size;
+    size = sv.getSize();
     // Get a pointer to other list's first node
     node *otherCurr = sv.firstnon0;
     // Use prev and curr to create the copy
@@ -53,6 +52,11 @@ void SparseVector::copyList(const SparseVector &sv)
         prev = curr;
         otherCurr = otherCurr->next;
     }
+    if (sv.firstnon0 == 0)
+    {
+        firstnon0 = 0;
+    }
+    //checkListOrder();
 }
 
 // helper method to set a nonzero value for a node with specified index
@@ -60,40 +64,50 @@ void SparseVector::setNonzeroElem(int idx, int val)
 {
     assert(val != 0);
     node *curr = firstnon0;
-    node *prev;
+    node *prev = 0;
 
-    // go through the sparse vector
-    while (curr->index < idx && curr->next != 0)
-    {
-        prev = curr;
-        curr = curr->next;
-    }
+    //checkListOrder();
 
-    // if we find the index
-    if (curr->index == idx)
+    if (firstnon0 != 0)
     {
-        curr->value = val;
+        // go through the sparse vector
+        while (curr->index < idx && curr->next != 0)
+        {
+            prev = curr;
+            curr = curr->next;
+        }
+
         //checkListOrder();
+        // if we find the index
+        if (curr->index == idx)
+        {
+            curr->value = val;
+        }
+        // if we can't find the index and are at the beginning of the list
+        else if (firstnon0->index > idx)
+        {
+            node *newnode = new node(idx, val, firstnon0);
+            firstnon0 = newnode;
+        }
+        // if we can't find the index and are at the end of the list
+        else if (curr->next == 0)
+        {
+            node *newnode = new node(idx, val);
+            curr->next = newnode;
+        }
+        // if we can't find the index and are in the middle of the list
+        else if (curr->index > idx)
+        {
+            node *newnode = new node(idx, val);
+            prev->next = newnode;
+            newnode->next = curr;
+        }
     }
-    // if we can't find the index and are at the beginning of the list
-    else if (firstnon0->index > idx)
+    else
     {
-        node *newnode = new node(idx, val, firstnon0);
-        firstnon0 = newnode;
+        firstnon0 = new node(idx, val);
     }
-    // if we can't find the index and are at the end of the list
-    else if (curr->next == 0)
-    {
-        node *newnode = new node(idx, val);
-        curr->next = newnode;
-    }
-    // if we can't find the index and are in the middle of the list
-    else if (curr->index > idx)
-    {
-        node *newnode = new node(idx, val);
-        prev->next = newnode;
-	newnode->next = curr;
-    }
+    //checkListOrder();
 }
 
 // helper method to delete a node from the list
@@ -103,14 +117,14 @@ void SparseVector::removeElem(int idx)
     node *prev;
 
     // go through the sparse vector
-    while (curr != 0 and curr->index < idx)
+    while (curr != 0 && curr->index < idx)
     {
         prev = curr;
         curr = curr->next;
     }
 
     // if we find the index and are at the middle of the list
-    if (curr->index == idx)
+    if (curr != 0 && curr->index == idx)
     {
         prev->next = curr->next;
         delete curr;
@@ -124,7 +138,7 @@ void SparseVector::removeElem(int idx)
 SparseVector::SparseVector(const SparseVector &sv)
 {
     this->copyList(sv);
-    checkListOrder();
+    //checkListOrder();
 }
 
 // Destructor
@@ -143,25 +157,24 @@ int SparseVector::getSize() const
 // Get the value at the specified node
 int SparseVector::getElem(int idx) const
 {
-    //checkListOrder();
     node *curr = firstnon0;
-
-    while(curr->index < idx)
+    //checkListOrder();
+    if (curr != 0)
     {
-        curr = curr->next;
-    }
+        while(curr->index < idx && curr->next != 0)
+        {
+            curr = curr->next;
+        }
 
-    // if we find the desired index, return the value
-    if (curr->index == idx)
-    {
-        return curr->value;
+        // if we find the desired index, return the value
+        if (curr->index == idx)
+        {
+            //checkListOrder();
+            return curr->value;
+        }
     }
-
-    // if the desired index is not in list, return 0 too;
-    else
-    {
-        return 0;
-    }
+    //checkListOrder();
+    return 0;
 }
 
 void SparseVector::setElem(int idx, int val)
@@ -190,21 +203,23 @@ SparseVector& SparseVector::operator= (const SparseVector &sv)
     return *this;
 }
 
-void SparseVector::checkListOrder()
+void SparseVector::checkListOrder() const
 {
-    node *curr = firstnon0;
+    node *test = firstnon0;
     int idx = -1;
-    while (curr->next != 0)
+    while (test != 0)
     {
-        if (idx < curr->index)
+        //printf("apparently so \n");
+
+        if (idx < test->index)
         {
-            idx = curr->index;
+            idx = test->index;
         }
         else
         {
-            printf("Error in index progression from index %d to index %d \n", idx, curr->index);
+            printf("Error in index progression from index %d to index %d \n", idx, test->index);
         }
-        curr = curr->next;
+        test = test->next;
     }
 }
 
