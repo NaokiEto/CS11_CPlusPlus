f45b3b183b15f688b46a06fa156f432e059f07ff
Naoki Eto <thinkflow12@yahoo.com>
2013-11-01 04:28:15 -0400

    some changes to lab4

--- lab4/SparseVector.hh
+++ lab4/SparseVector.hh
@@ -8,15 +8,17 @@ class SparseVector
         int index;
         int value;
         node *next;
+        node(int idx, int val, node *nxt = 0) :
+        index(idx), value(val), next(nxt) { }
     };
     int size;
 
     // node-pointer to the first nonzero element in the list
     node *firstnon0;
-    node *arr;
     void clear();
     void copyList(const SparseVector &sv);
     void setNonzeroElem(int idx, int val);
+    void removeElem(int idx);
 
     public:
     // Constructors
@@ -33,6 +35,9 @@ class SparseVector
     // Accessor member function
     int getSize() const;
     int getElem(int idx) const;
+
+    // Mutators
+    void setElem(int idx, int val);
 
     // Overloading the assignment operator
     SparseVector& operator=(const SparseVector &sv);
